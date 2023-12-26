8a919aacff02d9d92bfeb94690c433547dd2fc10
Garrett Wyler <grwyler@gmail.com>
2023-12-25 19:42:46 -0500

    removed server export. added 404 page

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 // next.config.js
 module.exports = {
-  server: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 3000,
-  },
   basePath: process.env.NODE_ENV === "production" ? "/grwyler/LiftLogic" : "",
 };
